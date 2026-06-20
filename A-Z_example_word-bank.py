29b8f31fb6fa458213530dcd2e31758b7540a612
TheAstrographer <theeastrophysicist@hotmail.com>
2026-06-20 17:14:37 -0600

    Create A-Z_example_word-bank.py

---
+++ A-Z_example_word-bank.py
@@ -0,0 +1,73 @@
+WORD_BANK = {
+    "A": ["abyssal", "adamantine", "aetheric", "acute", "anguished", "arcane", "assassinate", "austere", "autopsy", "axiom", 
+          "abhor", "abstruse", "acrimonious", "alacrity", "anathema", "antithesis", "apocryphal", "assiduous", "auspicious", "axiomatically"],
+    
+    "B": ["barbaric", "belligerent", "blade", "blood", "bludgeon", "brutal", "burn", "break", "bone-deep", "behemoth",
+          "bellicose", "benighted", "bifurcate", "bilious", "bombastic", "brooding"],
+    
+    "C": ["cataclysm", "chthonic", "concatenation", "crystalline", "cauterize", "cipher", "cleave", "cosmic", "crux",
+          "cacophony", "cadaverous", "capitulate", "catharsis", "chimerical", "circumscribe", "cogent", "conflagration"],
+    
+    "D": ["demolish", "density", "dialectic", "dissection", "dissonance", "dread", "devour", "delta", "doxa",
+          "deleterious", "demagogue", "diaphanous", "didactic", "disparate", "dissonant", "dour", "draconian"],
+    
+    "E": ["elegant", "entropy", "epistemic", "eviscerate", "exsanguinate", "eclipse", "embodiment", "eternal",
+          "ebullient", "eclectic", "effervescent", "egregious", "elucidate", "emaciated", "enervate", "ephemeral", "equivocal", "esoteric", "euphemism", "exacerbate"],
+    
+    "F": ["fracture", "feral", "forge", "fuck", "fury", "frozen", "flux", "flay", "fulcrum",
+          "facetious", "fallacious", "fatuous", "feckless", "felicitous", "ferocity", "fissure", "flagrant", "fugitive"],
+    
+    "G": ["gut", "gradient", "grasp", "ghost", "glacial", "gorge", "gravitational",
+          "garrulous", "germinal", "gossamer", "gregarious", "guttural"],
+    
+    "H": ["hegemonic", "hewn", "harrowing", "horizon", "hunger", "hammer",
+          "hackneyed", "halcyon", "harangue", "heinous", "hermetic", "heterogeneous", "hubris"],
+    
+    "I": ["ineffable", "inferno", "interstice", "incarnate", "impale", "infinite", "ignition",
+          "iconoclast", "idiosyncratic", "ignominious", "immutable", "impecunious", "impetuous", "inchoate", "incisive", "indefatigable", "intransigent", "invective"],
+    
+    "J": ["jagged", "judgment", "juxtaposition", "jejune", "jocular", "judicious"],
+    
+    "K": ["knife", "kinetic", "killing", "keystone", "knell", "knotty"],
+    
+    "L": ["liminal", "lacerate", "lightning", "latent", "leviathan", "loom",
+          "labyrinthine", "lachrymose", "laconic", "languid", "largesse", "lethargic", "lithe", "lugubrious"],
+    
+    "M": ["manifest", "maul", "mirror", "metamorphosis", "monolith", "murderous",
+          "machination", "maelstrom", "magnanimous", "malevolent", "malediction", "malleable", "mercurial", "meticulous", "miscreant", "mitigate", "moribund"],
+    
+    "N": ["nihil", "neural", "naked", "nexus", "nocturnal",
+          "nefarious", "neologism", "nettlesome", "nondescript", "notorious", "nugatory"],
+    
+    "O": ["ontological", "obliterate", "obsidian", "orchestrate", "oracle",
+          "obdurate", "obfuscate", "oblique", "obstreperous", "odious", "onerous", "opprobrium", "ostensible", "ostracize"],
+    
+    "P": ["perspicacious", "pulverize", "precipice", "phantom", "primal", "paradox",
+          "palliate", "panacea", "paradigmatic", "paragon", "parsimonious", "pejorative", "perfidious", "pernicious", "pertinacious", "phlegmatic", "piquant", "plethora", "portentous", "proclivity", "prodigious", "prosaic", "pugnacious"],
+    
+    "Q": ["quasar", "quivering", "quintessential", "quell", "quixotic", "quotidian"],
+    
+    "R": ["raw", "ravage", "resonant", "rupture", "razor", "RLHF", "reverberate",
+          "rancorous", "rapacious", "rebuke", "recalcitrant", "redolent", "refractory", "replete", "reprobate", "resplendent", "reticent", "rhetoric", "ribald", "risible"],
+    
+    "S": ["scalpel", "shatter", "stone", "surmounted", "steer", "silence", "sharp", "sever", "sovereign",
+          "sagacious", "salient", "sanguine", "scathing", "schism", "scintilla", "serendipity", "soporific", "specious", "spurious", "stentorian", "strident", "subterfuge", "supercilious", "surfeit"],
+    
+    "T": ["thrust", "token", "tension", "tempest", "threshold", "tyranny",
+          "taciturn", "tantamount", "tautological", "temerity", "tenacious", "tenebrous", "timorous", "torpid", "transcendental", "transmute", "trenchant", "turbid", "turgid"],
+    
+    "U": ["unyielding", "unfurl", "unholy", "umbra",
+          "ubiquitous", "unassailable", "unctuous", "undulate", "unfettered", "unmitigated", "unprecedented", "usurp"],
+    
+    "V": ["vector", "visceral", "void", "vortex", "venom", "verve",
+          "vacillate", "vapid", "vehement", "veracity", "verbose", "vicissitude", "vitriolic", "vociferous", "volatile", "voluble"],
+    
+    "W": ["wound", "wrath", "wild", "weight", "whiplash",
+          "waning", "wanton", "wary", "welter", "wily", "winsome", "wither"],
+    
+    "X": ["xenolith", "xanthic", "x-rayed", "xenophobia", "xerophyte"],
+    
+    "Y": ["yielding", "yawning", "yearning", "yeoman", "yoke"],
+    
+    "Z": ["zenith", "zero-point", "zeitgeist", "zephyr", "zealot"]
+}
